909159a28cd8f437e29831e1114d21389c22bbb6
suryamudti <suryamudti0128@gmail.com>
2020-06-20 19:05:37 +0700

    add slider value

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct ContentView: View {
     
     @State var alertIsVisible: Bool = false
+    @State var sliderValue: Double = 50.0
     
     var body: some View {
         VStack {
@@ -27,7 +28,7 @@ struct ContentView: View {
             // Slider row
             HStack {
                 Text("1")
-                Slider(value: .constant(50))
+                Slider(value: self.$sliderValue, in: 1...100)
                 Text("100")
             }
             Spacer()
