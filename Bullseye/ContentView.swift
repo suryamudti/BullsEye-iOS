d9f7bd992925fbd23d1be5cbda5ac2d3cbf924eb
suryamudti-tiket <suryamudti.salmat@tiket.com>
2020-06-20 16:23:17 +0700

    Initial Commit

---
+++ Bullseye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bullseye
+//
+//  Created by Suryamudti Salmat on 20/06/20.
+//  Copyright © 2020 Suryamudti Salmat. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
