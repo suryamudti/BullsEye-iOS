aa09220c3bb1a61374b433c45d9e2e67eee077d6
suryamudti <suryamudti0128@gmail.com>
2020-06-21 14:39:27 +0700

    add LabelStyle

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -16,23 +16,30 @@ struct ContentView: View {
     @State var score = 0
     @State var round = 1
     
+    struct LabelStyle: ViewModifier {
+        func body(content: Content) -> some View {
+            return content
+            .foregroundColor(Color.white)
+            .shadow(color: Color.black, radius: 5, x: 2, y: 2)
+            .font(Font.custom("Arial Rounded MT bOLD", size: 18))
+        }
+    }
+    
     var body: some View {
         VStack {
             Spacer()
             // Target row
             HStack {
-                Text("Put the bullseye as close as you can to: ")
-                    .foregroundColor(Color.white)
-                    .shadow(color: Color.black, radius: 5, x: 2, y: 2)
-                Text("\(self.target)")
+                Text("Put the bullseye as close as you can to: ").modifier(LabelStyle())
+                Text("\(self.target)").modifier(LabelStyle())
             }
             Spacer()
             
             // Slider row
             HStack {
-                Text("1")
+                Text("1").modifier(LabelStyle())
                 Slider(value: self.$sliderValue, in: 1...100)
-                Text("100")
+                Text("100").modifier(LabelStyle())
             }
             Spacer()
             
@@ -64,10 +71,10 @@ struct ContentView: View {
                     Text("Start Over")
                 }
                 Spacer()
-                Text("Score:")
+                Text("Score:").modifier(LabelStyle())
                 Text("\(score)")
                 Spacer()
-                Text("Round")
+                Text("Round").modifier(LabelStyle())
                 Text("\(round)")
                 Spacer()
                 Button(action: {}){
